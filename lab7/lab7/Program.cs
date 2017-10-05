67db458c4a4a96f8db2f6cae355ce841de42dbb1
minwichita <59030206@kmitl.ac.th>
2017-10-05 22:29:12 +0700

    edmin-fields5

--- lab7/lab7/Program.cs
+++ lab7/lab7/Program.cs
@@ -1,36 +1,22 @@
 ﻿
-using System;
-
-namespace ConsoleApp5
+class Student
 {
-    class Program
+    private string name;
+    private string id;
+    private float gpa;
+    public string Name
     {
-        static void Main(string[] args)
-        {
-            Point myPoint = new Point();  // create new object
-            myPoint.X = 10;
-            myPoint.Y = 10;
-            Console.WriteLine("myPoint.x = {0}", myPoint.X);
-            Console.WriteLine("myPoint.y = {0}", myPoint.Y);
-            Console.ReadLine();
-        }
+        get { return name; }
+        set { name = value; }
     }
-
-    class Point          // class name
+    public string ID
     {
-        private int x;    // fields
-        private int y;
-
-        public int X      // properties
-        {
-            get { return x; }
-            set { x = value; }
-        }
-
-        public int Y
-        {
-            get { return y; }
-            set { y = value; }
-        }
+        get { return id; }
+        set { id = value; }
+    }
+    public float GPA
+    {
+        get { return gpa; }
+        set { gpa = value; }
     }
 }
